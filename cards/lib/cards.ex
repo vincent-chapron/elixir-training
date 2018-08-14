707f345e635fa530a1ee5c9c7e0307263534078e
Vincent CHAPRON <vincent@chapron.io>
2018-08-14 22:19:37 +0200

    Generate a project with mix command and define a first method

---
+++ cards/lib/cards.ex
@@ -0,0 +1,7 @@
+defmodule Cards do
+  def hello do # define a new method inside our module
+    "Hi there !"  # will automatically return this string (implicite return)
+                  # last value will automatically get returned
+                  # same as `return "Hi there !"`
+  end
+end
