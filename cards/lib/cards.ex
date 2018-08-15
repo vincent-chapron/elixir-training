ecaedeb15cf419150d82c9bbe2b31bb510ed3ed9
Vincent CHAPRON <vincent@chapron.io>
2018-08-15 18:27:06 +0200

    use file system and case pattern matching

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -25,4 +25,16 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size) # pattern matching : { hand, the_rest } = Cards.deal(deck, 2)
   end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "That file does not exist"
+    end
+  end
 end
